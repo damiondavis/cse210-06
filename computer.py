587c17223f1466fd23a3e6726dad2f07cd0b132d
SethHartman13 <familyhartseth@aol.com>
2022-12-06 17:30:50 -0700

    basic risk/decision management created

--- computer.py
+++ computer.py
@@ -6,8 +6,8 @@ class Computer:
     def __init__(self):
         self._base_risk = .5
         self._messages = ['Drat! I busted', 'Yay! I scored over 1000 points', 'Haha, you busted']
-        self._total_points = 0
-        self._round_points = 0
+        self._total_points = int(0)
+        self._round_points = int(0)
 
     def return_risk(self):
         return self._base_risk
@@ -24,32 +24,38 @@ class Computer:
             self._base_risk = 1
 
     def risk_control(self):
-        player_points = p.get_player_total_points()
-        if self._points - player_points >= 2000:
-            self.modify_risk(.8)
-        elif self._points - player_points >= 1000:
+        # The Computer will raise/lower its risk based upon whether it is further ahead or further behind.
+        _player_points = p.get_player_total_points()
+        if self._total_points - _player_points >= 2000:
+            self.modify_risk(1.2)
+        elif self._total_points - _player_points >= 1000:
+            self.modify_risk(1.1)
+        elif _player_points - self._total_points >= 1000:
             self.modify_risk(.9)
+        elif _player_points - self._total_points >= 2000:
+            self.modify_risk(.8)
 
     def logic_determine(self):
         risk = self.return_risk()
-        round_points = self._round_points
+
+        # The Computer gets riskier as its risk level is higher and vice versa
         if risk <= .4:
-            if round_points >= 1000:
+            if self._round_points >= 1000:
                 return "stop"
-            if round_points < 1000:
+            if self._round_points < 1000:
                 return "roll"
         if risk > .4 and risk <= .6:
-            if round_points >= 1250:
+            if self._round_points >= 1250:
                 return "stop"
-            if round_points < 1250:
+            if self._round_points < 1250:
                 return "roll"
         if risk > .6 and risk <= .8:
-            if round_points >= 1750:
+            if self._round_points >= 1750:
                 return "stop"
-            if round_points < 1750:
+            if self._round_points < 1750:
                 return "roll"
         else:
-            if round_points >= 2500:
+            if self._round_points >= 2500:
                 return "stop"
-            if round_points < 2500:
+            if self._round_points < 2500:
                 return "roll"
