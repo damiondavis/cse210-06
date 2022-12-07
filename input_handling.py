122f0c1f774462a5a9a4ef6c2f816bca1ec80179
americanfootball99 <113465103+americanfootball99@users.noreply.github.com>
2022-12-07 09:56:12 -0700

    Detailed what inputs we need

--- input_handling.py
+++ input_handling.py
@@ -7,3 +7,12 @@ class Choice:
         input('Choose a die to keep.')
         if choice.lower() != 'done':
             choice = input('Choose another die or type DONE to finish choosing.')
+            
+# Inputs the player should be allowed to make
+# A list of all the choices they have available (like a controls menu)
+# See the rules displayed on screen (this will be grabbed from the txt file)
+# See the score conditions diplayed on screen
+# See their current score
+# See what dice they have kept so far
+# Choose which die they want to keep (one at a time)
+# Finish choosing dice
