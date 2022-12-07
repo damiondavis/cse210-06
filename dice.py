38d980bd62717fd2c5941aef79e9003b4e750c19
americanfootball99 <113465103+americanfootball99@users.noreply.github.com>
2022-12-07 12:21:00 -0700

    colors

--- dice.py
+++ dice.py
@@ -1,7 +1,9 @@
 import random as r
 from input_handling import Choice
+from termcolor import colored
 
 die_value = 0
+die_numbers = [1,2,3,4,5,6]
 
 # Player values
 rolls = []
@@ -17,14 +19,15 @@ class D:
             print('You have no more dice to roll.')
             choice = 'done'
         else:
-            while rolls < (6 - kept_rolls):
+            while rolls < (6 - kept_rolls.count()):
                 rolls.append(r.randint(1,6))
-            print(*rolls, sep = ' ')
+            print(colored(*die_numbers[0:(kept_rolls.count() -6)], 'red'))
+            print(colored(*rolls, 'green'), sep = ' ')
     
     def bot_roll(bot_rolls,bot_kept_rolls):
         if bot_kept_rolls.count() == 6:
             print('no more dice for bot')
         else:
-            while bot_rolls < (6 - bot_kept_rolls):
+            while bot_rolls < (6 - bot_kept_rolls.count()):
                 bot_rolls.append(r.randint(1,6))
             print(*bot_rolls, sep = ' ')
