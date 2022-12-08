ef561bf47373331e26720cdceede4d1eeef93599
damiondavis <114271945+damiondavis@users.noreply.github.com>
2022-12-08 11:14:00 -0700

    Update input_handling.py

--- input_handling.py
+++ input_handling.py
@@ -1,19 +1,47 @@
 import dice as d
-
+from score import Score
 choice = 'done'
 
-class Choice:
-    def die_selection():
-        input('Choose a die to keep.')
-        if choice.lower() != 'done':
-            choice = input('Choose another die or type DONE to finish choosing.')
-            
-# Inputs the player should be allowed to make
-# A list of all the choices they have available (like a controls menu)
-# See the rules displayed on screen (this will be grabbed from the txt file)
-# See the score conditions diplayed on screen
-# See their current score
-# See what dice they have kept so far
-# Choose which die they want to keep (one at a time)
-# Finish choosing dice
-#
+#Assign the total rolls and kept rolls to lists in dice file
+
+class Choice(d, Score):
+    kept_rolls = d.kept_rolls
+    rolls = d.rolls
+
+
+    def die_selection(self):
+
+        #Open the rules file and print it at the top of the terminal each time the user has the option to select dice
+        with open("rules.txt", "r", encoding="utf-8") as rules:
+            for line in rules:
+                print(line.strip())
+
+        #Display the current rolls to the player
+        print(f'Your roll looks like this {d.rolls}')
+        #Allows the user to enter which dice #'s they want to keep, seperated by commas
+        keep_input = input('Which dice do you want to keep (comma seperated: (e.g 1,1,5)? ')
+        split_input = keep_input.split(',')
+
+        #Adds the user's selection to the kept rolls list
+        split_input_int = [int(item) for item in split_input]
+        for die in split_input_int:
+            d.kept_rolls.append(die)
+
+        #if the user types 'done' they will be finished selecting dice
+        if keep_input == 'done':
+            return d.kept_rolls
+        else:
+            print('Invalid input, please try again.')
+
+        #remove the selected dice from the current rolls
+        for value in split_input_int:
+            if value in d.rolls:
+                d.rolls.remove(value)
+        return d.rolls
+
+        #This is where we would want to reroll the remaining die - JW
+
+
+    def show_updates(total_points):
+        print(f'Your total score is {Score.total_points}')
+        print(f'Your kept rolls are {d.kept_rolls}')
